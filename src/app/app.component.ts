7b2f600b9efd6e42e89713878518eaf79e48a604
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2025-01-18 15:05:27 -0500

    (UPDATE) The components were create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent, AboutMeComponent, ContactComponent, FooterComponent,
+    HomeComponent, NavComponent, ProjectsComponent, SkillsComponent
+   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
