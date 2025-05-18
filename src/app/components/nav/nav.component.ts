69dffd0273388be126fb3d2682d78fd6f48b57b0
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2025-05-17 20:45:45 -0500

    Componente nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  isDarkMode: boolean = true;
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('light-mode', !this.isDarkMode);
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+  }
+  
+  ngOnInit(): void {
+    const savedMode = localStorage.getItem('darkMode');
+    this.isDarkMode = savedMode === null ? true : savedMode === 'true';
+    document.body.classList.toggle('light-mode', !this.isDarkMode);
+  }
 }
