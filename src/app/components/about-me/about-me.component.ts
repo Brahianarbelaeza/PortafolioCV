df72c97570f73ff51816b4b5e73e1d22c1acecea
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2025-05-17 22:45:29 -0500

    (UPDATE) add bg to home

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
+import { environment } from '../../env/enviroment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
 
+  isDarkMode = environment.COLOR_MODE === 'dark';
   themeStyles = this.computeStyles();
 
+  ngOnInit(): void {
+    const saved = localStorage.getItem('darkMode');
+    this.isDarkMode = saved === null ? this.isDarkMode : saved === 'true';
+    document.body.classList.toggle('light-mode', !this.isDarkMode);
+    this.themeStyles = this.computeStyles();
+  }
 
   private computeStyles() {
     return {
-      backgroundColor: '#E0E8F6',
-      textColor: '#333',
-      fontFamily: 'Montserrat, sans-serif',
-      fontSize: '18px'
+      bgColor: this.isDarkMode ? environment.BG_DARK_COLOR : environment.BG_LIGHT_COLOR,
+      textColor: this.isDarkMode ? environment.NAV_DARK_TEXT_COLOR : environment.NAV_LIGHT_TEXT_COLOR
     };
   }
-
   
 }
